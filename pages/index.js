13006b6d3c79bb07012781835bbb5fe58ecc89d9
Ronaldo T. Pinto <ronaldotpinto@gmail.com>
2024-12-14 19:56:22 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Obrigado por tudo que tens feito por mim nesse momento desafiador, Te amo!</h1>
+    return <h1>
+            Obrigado por tudo que tens feito por mim nesse momento desafiador.<br />
+            Te amo! &#128156;
+        </h1>
 }
 
 export default Home;
